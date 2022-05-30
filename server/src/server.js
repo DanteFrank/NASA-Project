f3efc62ce959b1ddd4704a7eac4254634cba488a
DanteFrank <davidfrankoziwo@gmail.com>
2022-05-30 09:35:28 +0100

    API, ROUTE and Model

--- server/src/server.js
+++ server/src/server.js
@@ -4,8 +4,16 @@ const app = require("./app");
 
 const PORT = process.env.PORT || 8000;
 
+const { loadPlanetsData } = require("./models/planets.model");
+
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server running and listening on ${PORT}`);
-});
+async function startServer() {
+  await loadPlanetsData();
+
+  server.listen(PORT, () => {
+    console.log(`Server running and listening on ${PORT}`);
+  });
+}
+
+startServer();
